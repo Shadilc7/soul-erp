fa8c6013f6542dd75011f0ea9973a99f70398bfa
shadil <shadilc7@gmail.com>
2026-04-05 20:41:00 +0530

    beya landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,24 +2,5 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    return unless user_signed_in?
-
-    path = case current_user.role
-    when "master_admin"
-      admin_root_path
-    when "institute_admin"
-      institute_admin_root_path
-    when "trainer"
-      trainer_portal_root_path
-    when "participant"
-      if current_user.participant.present?
-        participant_portal_root_path
-      else
-        sign_out current_user
-        root_path
-      end
-    end
-
-    redirect_to path if path.present?
   end
 end
