494b3d79b946e2e8218a15ff9b47e2b5ddd9fe2f
shadil <shadilc7@gmail.com>
2026-06-28 13:21:03 +0530

    fix: redirect issue for invalid user resolved

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,12 +1,14 @@
 class CustomFailure < Devise::FailureApp
   def redirect_url
-    root_path
+    new_user_session_path
   end
 
   def respond
     if http_auth?
       http_auth
     else
+      # Set the flash alert so the user sees the error message on the login page
+      flash[:alert] = i18n_message unless flash[:alert].present?
       redirect
     end
   end
