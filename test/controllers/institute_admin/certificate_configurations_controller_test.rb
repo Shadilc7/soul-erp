d033407c82cc9040b412e71bfa4186c0a45074a6
shadil <shadilc7@gmail.com>
2026-07-01 22:10:57 +0530

    fix: ferrum pdf update

--- test/controllers/institute_admin/certificate_configurations_controller_test.rb
+++ test/controllers/institute_admin/certificate_configurations_controller_test.rb
@@ -1,33 +1,66 @@
 require "test_helper"
 
 class InstituteAdmin::CertificateConfigurationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @admin = users(:one)
+    @institute = @admin.institute
+    @certificate_configuration = certificate_configurations(:one)
+    sign_in @admin
+  end
+
   test "should get index" do
-    get institute_admin_certificate_configurations_index_url
+    get institute_admin_certificate_configurations_url
     assert_response :success
   end
 
   test "should get new" do
-    get institute_admin_certificate_configurations_new_url
+    get new_institute_admin_certificate_configuration_url
     assert_response :success
   end
 
-  test "should get create" do
-    get institute_admin_certificate_configurations_create_url
-    assert_response :success
+  test "should create certificate_configuration" do
+    assert_difference("CertificateConfiguration.count") do
+      post institute_admin_certificate_configurations_url, params: {
+        certificate_configuration: {
+          name: "New Config",
+          details: "Some details",
+          duration_period: 10,
+          status: "active"
+        }
+      }
+    end
+    assert_redirected_to institute_admin_certificate_configurations_path
   end
 
   test "should get edit" do
-    get institute_admin_certificate_configurations_edit_url
+    get edit_institute_admin_certificate_configuration_url(@certificate_configuration)
     assert_response :success
   end
 
-  test "should get update" do
-    get institute_admin_certificate_configurations_update_url
-    assert_response :success
+  test "should update certificate_configuration" do
+    patch institute_admin_certificate_configuration_url(@certificate_configuration), params: {
+      certificate_configuration: {
+        name: "Updated Name",
+        duration_period: 10
+      }
+    }
+    assert_redirected_to institute_admin_certificate_configurations_path
+    @certificate_configuration.reload
+    assert_equal "Updated Name", @certificate_configuration.name
   end
 
-  test "should get destroy" do
-    get institute_admin_certificate_configurations_destroy_url
-    assert_response :success
+  test "should destroy certificate_configuration" do
+    config_to_destroy = CertificateConfiguration.create!(
+      name: "To Destroy",
+      duration_period: 10,
+      institute: @institute,
+      status: :active
+    )
+    assert_difference("CertificateConfiguration.count", -1) do
+      delete institute_admin_certificate_configuration_url(config_to_destroy)
+    end
+    assert_redirected_to institute_admin_certificate_configurations_path
   end
 end
